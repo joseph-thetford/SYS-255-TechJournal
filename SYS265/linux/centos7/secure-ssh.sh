d3d5113b2edbd7b5f8e6af9835c06c75d975d240
joseph-thetford <joseph.thetford@mymail.champlain.edu>
2020-02-18 14:24:18 -0500

    added script and public key

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author joe
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "ENTER CODE HERE"
