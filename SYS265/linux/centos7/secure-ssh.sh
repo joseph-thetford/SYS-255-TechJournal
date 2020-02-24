9259e4f411a0fac852f065f13ff1fa1efed07d00
joseph-thetford <joseph.thetford@mymail.champlain.edu>
2020-02-24 23:48:03 +0000

    update to secure-ssh.sh

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -1,6 +1,12 @@
 #secure-ssh.sh
-#author joe
+#author joseph-thetford
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ENTER CODE HERE"
+new_user=$1
+sudo useradd -m -d /home/$new_user -s /bin/bash $new_user
+sudo mkdir /home/$new_user/.ssh
+sudo cp /home/joe/Tech-Journal/SYS265/linux/public-keys/id_rsa.pub /home/$new_user/.ssh/authorized_keys
+sudo chmod 700 /home/$new_user/.ssh
+sudo chmod 600 /home/$new_user/.ssh/authorized_keys
+sudo chown -R $new_user:$new_user /home/$new_user/.ssh
